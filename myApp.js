690e0cae2d0a92f4974485e57feb18dbe95bead7
kushagra_secomind <kushagra.varma@secomind.ai>
2024-07-21 17:25:09 +0530

    server sending html file and static file , the html file searchfor css file in public folder and then app.use comes into action  and then express.statis

--- myApp.js
+++ myApp.js
@@ -1,38 +1,14 @@
 let express = require('express');
 let app = express();
 
+pathToStaticFile = __dirname+"/public"
+pathToHtmlFile = __dirname+"/views/index.html"
 
+app.get('/',(req,res)=>{
+    res.sendFile(pathToHtmlFile)
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.use('/public',express.static(pathToStaticFile))
 
 
 

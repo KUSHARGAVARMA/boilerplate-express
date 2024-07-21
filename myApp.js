7342e01a2a56ce42462677b5c79ed7d0f9eec11d
kushagra_secomind <kushagra.varma@secomind.ai>
2024-07-21 18:03:39 +0530

    added a middleware logger

--- myApp.js
+++ myApp.js
@@ -5,16 +5,24 @@ require('dotenv').config()
 pathToStaticFile = __dirname+"/public"
 pathToHtmlFile = __dirname+"/views/index.html"
 
-let message="Hello json"
+
+//middleware for all the methods printing the logs in manner method path - ip
+app.use((req,res,next)=>{
+    console.log(req.method+" "+req.path+" "+"-"+" "+req.ip);
+    next();
+})
 // serving html file to first route 
 app.get('/',(req,res)=>{
     res.sendFile(pathToHtmlFile)
 })
-//Middleware call for public folder
+
+//Middleware call on public route 
 app.use('/public',express.static(pathToStaticFile))
+
 //adding a  json to be sent on the /json route 
 app.get('/json',(req,res)=>{
    styleCase = process.env.MESSAGE_STYLE
+    console.log(styleCase);
     if(styleCase === "uppercase"){
 
     res.send(
@@ -28,7 +36,6 @@ app.get('/json',(req,res)=>{
 
     }    
 })
-
 
 
  module.exports = app;
